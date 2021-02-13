17bd39c19da31570cdba42da567160cd1b07c314
SHARATH KUMAR B <sharath1981@gmail.com>
2021-02-13 15:22:01 +0530

    Do not Annotate Entity classes with Lombok's Data, ToString, EqualsAndHashCode Lombok ToString invokes the getter methods which negates the Lazy Loading

--- src/main/java/com/datajparest/resource/Person.java
+++ src/main/java/com/datajparest/resource/Person.java
@@ -15,9 +15,11 @@ import javax.persistence.Id;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 public class Person {
 	
